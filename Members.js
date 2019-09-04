9bdf0fd46635e81dbbc2102adc6ac51065194977
Ayako <ayakosayama1987@gmail.com>
2019-09-04 11:45:05 -0700

    Express

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 1,
+    name: "Syllinda",
+    email: "syllinda@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Shota",
+    email: "shota@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Eugene",
+    email: "eugene@gmail.com"
+  },
+];
+
+module.exports = members;
---
+++ routes/api/members.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+
+const members = require('../../Members');
+
+
+router.get('/', (req, res) => 
+  res.json(members)
+);
+
+router.get('/:id', (req, res) => {
+  //returns boolean
+  const found = members.some(
+    (member) => member.id === parseInt(req.params.id)
+  )
+
+  if(found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)))
+
+  } else {
+    res.status(400).json({ msg: `member with ${req.params.id}: id is not found.`})
+  }
+})
+
+router.post('/', (req, res) => {
+  console.log('request body', req.body)
+  const newData = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  members.push(newData);
+  res.json(members);
+})
+
+router.put('/:id', (req, res) => {
+ 
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router;
