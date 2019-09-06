5c7b4c12ad4ced4c1c48c7d3cdd54a903e204e8b
Ayako <ayakosayama1987@gmail.com>
2019-09-06 06:52:48 -0700

    commit to heroku

--- index.js
+++ index.js
@@ -34,5 +34,13 @@ connection.once('open', () => {
 app.use('/api/members', require('./routes/api/members'));
 app.use('/api/exercises', require('./routes/api/exercises'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build'))); //deploy
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')); // relative path
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
