a908bd0489933c2bac4983735e9b76cbf36af200
Ayako <ayakosayama1987@gmail.com>
2019-09-06 10:58:44 -0700

    push changes

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.get('/api', (req, res) =>
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // );
 
-
 //Set Static Folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,6 +32,15 @@ connection.once('open', () => {
 // Members API Routes
 app.use('/api/members', require('./routes/api/members'));
 app.use('/api/exercises', require('./routes/api/exercises'));
+
+// Set Static folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/frontend/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
